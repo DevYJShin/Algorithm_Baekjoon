f677942c144bf05787cddbd6e6d77a1a0bfebda8
yj-shin <fhqk1992@gmail.com>
2022-03-08 18:42:40 +0900

    second commit!

--- 2675_문자열반복.py
+++ 2675_문자열반복.py
@@ -4,4 +4,4 @@ for _ in range(n):
     cnt, word = input().split()
     for x in word:
         print(x*int(cnt), end='')  # end='' 옆으로 붙임
-    print()  # 줄넘김
+    print()                        # 줄넘김
