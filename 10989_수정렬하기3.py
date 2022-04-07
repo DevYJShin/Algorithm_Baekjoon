bbd249cf59cc1b6e8eed96b1bebd71d74d0e5c14
DevYJShin <yjshin.dev@gmail.com>
2022-04-07 20:05:16 +0900

    first commit!

---
+++ 10989_수정렬하기3.py
@@ -0,0 +1,26 @@
+import sys
+
+n = int(sys.stdin.readline())
+num_list = [0] * 10001
+
+for _ in range(n):
+    num_list[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if num_list[i] != 0:
+        for j in range(num_list[i]):
+            print(i)
+
+# 메모리 초과
+# import sys
+#
+# n = int(sys.stdin.readline())
+# num_list = []
+#
+# for _ in range(n):
+#     num_list.append(int(sys.stdin.readline()))
+#
+# sorted_list = sorted(num_list)
+#
+# for i in sorted_list:
+#     print(i)
