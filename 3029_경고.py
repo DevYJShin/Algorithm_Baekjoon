c34dda4b5319df38ce73c3cc7547e964ffa6860d
DevYJShin <yjshin.dev@gmail.com>
2022-04-13 06:16:51 +0900

    first commit!

--- 3029_경고.py
+++ 3029_경고.py
@@ -1,8 +1,14 @@
-def main():
-    h, m, s = map(int, input().split(":"))
-return h*3600+m*60+s ## runtime error
+h1, m1, s1 = map(int, input().split(':'))
+h2, m2, s2 = map(int, input().split(':'))
 
-now, throw = main(), main()
-time = now - throw + 86400 * (now >= throw) # 24h = 86400s
-print('%02d:%02d:%02d' %(time//3600, time%3600, time%3600%60))
+now = h1*60*60 + m1*60 + s1
+throw = h2*60*60 + m2*60 + s2
 
+time = throw - now if throw > now else throw - now + 24 * 60 * 60 
+# 86400 = 60 * 60 * 24 
+
+h = time // 60 * 60 # 1 hour -> 60m * 60s
+m = time // 60 % 60 
+s = time % 60
+
+print("%02d:%02d:%02d" % (h, m, s))
